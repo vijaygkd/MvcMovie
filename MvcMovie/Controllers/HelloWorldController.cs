95aae01e20af413e0406869525de4e8f01950a94
Vijay Gaikwad <vig@microsoft.com>
2016-10-10 20:44:26 -0700

    connect controller and views

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -9,17 +9,20 @@ namespace MvcMovie.Controllers
     public class HelloWorldController : Controller
     {
         // GET: HelloWorld
-        public string Index()
+        public ActionResult Index()
         {
-            return "This is my <b>default</b> action...";
+            return View();
         }
 
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome(string name, int ID = 1)
+        public ActionResult Welcome(string name, int numtimes = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+            ViewBag.Message = $"Hello {name}";
+            ViewBag.NumTimes = numtimes;
+
+            return View();
         }
     }
 }
